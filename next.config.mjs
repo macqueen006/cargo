131f9f725321ed2a0b22ee680e658eaa556c4282
godwin <65357127+macqueen006@users.noreply.github.com>
2024-10-02 11:09:04 +0100

    modified the path in config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  basePath: process.env.NODE_ENV === 'production' ? 'https://macqueen006.github.io/cargo' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://macqueen006.github.io/cargo/' : '',
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
