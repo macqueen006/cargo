c955ca3ec4a5250e85c8b8be6f612cb1d2cec84a
godwin <65357127+macqueen006@users.noreply.github.com>
2024-10-07 19:36:48 +0100

    added the CompanyLogos section to the home page

--- app/(home)/_components/CompanyLogos.tsx
+++ app/(home)/_components/CompanyLogos.tsx
@@ -1,71 +1,73 @@
 import Image from "next/image";
 import Link from "next/link";
+import {prefix} from "@/libs/util";
+
+const companyLogos = [
+    {
+        id: 1,
+        img: `${prefix}08/client-1-hover.jpg`,
+        logo: `${prefix}08/Client-1.png`
+    },
+    {
+        id: 2,
+        img: `${prefix}08/client-2-hover.jpg`,
+        logo: `${prefix}08/Client-2.png`
+    },
+    {
+        id: 3,
+        img: `${prefix}08/client-3-hover.jpg`,
+        logo: `${prefix}08/Client-3.png`,
+    },
+    {
+        id: 4,
+        img: `${prefix}08/client-4-hover.jpg`,
+        logo: `${prefix}08/Client-4.png`,
+    },
+    {
+        id: 5,
+        img: `${prefix}08/client-5-hover.jpg`,
+        logo: `${prefix}08/Client-5.png`,
+    },
+]
 
 const CompanyLogos = () => {
     return (
-      <section className="bg-[#12171b] relative">
-        <div className="flex items-center justify-center w-full flex-wrap">
-          {[
-            {
-              id: 1,
-              img: "08/client-1-hover.jpg",
-              logo: "08/Client-1.png",
-            },
-            {
-              id: 2,
-              img: "08/client-2-hover.jpg",
-              logo: "08/Client-2.png",
-            },
-            {
-              id: 3,
-              img: "08/client-3-hover.jpg",
-              logo: "08/Client-3.png",
-            },
-            {
-              id: 4,
-              img: "08/client-4-hover.jpg",
-              logo: "08/Client-4.png",
-            },
-            {
-              id: 5,
-              img: "08/client-5-hover.jpg",
-              logo: "08/Client-5.png",
-            },
-          ].map((item, index) => (
-            <div
-              key={index}
-              className="flex items-center justify-center w-full flex-wrap"
-            >
-              <div className="outline w-full group outline-[#FFFFFF26] mt-[1px] ml-[1px] relative grid grid-cols-1 grid-rows-1 place-items-center">
-                <span className="hover-logo group-hover:remove-logo relative col-start-1 col-end-1 row-start-1 row-end-1 z-[1]">
-                  <Image
-                    className="block max-w-full"
-                    src={item.img}
-                    alt="img"
-                    width={600}
-                    height={438}
-                  />
-                </span>
-                <span className="relative col-start-1 col-end-1 row-start-1 row-end-1 z-[2]">
-                  <Image
-                    className="inline-block"
-                    src={item.logo}
-                    alt="img"
-                    width={166}
-                    height={58}
-                  />
-                </span>
-                <Link
-                  href="#"
-                  className="w-full h-full absolute top-0 left-0 z-[1]"
-                  target="_blank"
-                />
-              </div>
+        <section className="bg-[#12171b] relative inline-block">
+            <div className="flex items-center w-full flex-wrap">
+                {companyLogos.map((item, index) => (
+                    <div key={index} className="sm:basis-1/3 lg:basis-1/5 flex items-center w-full flex-wrap border border-[#FFFFFF26] border-l-transparent">
+                        <div
+                            className="w-full group relative grid grid-cols-1 grid-rows-1 place-items-center ">
+                            <span
+                                className="hover-logo group-hover:remove relative col-start-1 col-end-1 row-start-1 row-end-1 z-[1]">
+                              <Image
+                                  className="block max-w-full"
+                                  src={item.img}
+                                  alt="img"
+                                  width={600}
+                                  height={438}
+                              />
+                            </span>
+                            <span className="relative col-start-1 col-end-1 row-start-1 row-end-1 z-[2]">
+                              <Image
+                                  className="inline-block"
+                                  src={item.logo}
+                                  alt="img"
+                                  width={166}
+                                  height={58}
+                              />
+                            </span>
+                            <Link
+                                href="#"
+                                className="w-full h-full absolute top-0 left-0 z-[1]"
+                                target="_blank"
+                            />
+                        </div>
+                    </div>
+                ))}
             </div>
-          ))}
-        </div>
-      </section>
+        </section>
     );
-  };
-  
+};
+
 export default CompanyLogos;
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -11,41 +11,41 @@ import Counters from "./_components/Counters";
 import OurDevelopers from "./_components/OurDevelopers";
 import Frame from "./_components/Frame";
 import PricingRange from "./_components/PricingRange";
-// import CompanyLogos from "./_components/CompanyLogos";
-import { prefix } from "@/libs/util";
+import CompanyLogos from "./_components/CompanyLogos";
+import {prefix} from "@/libs/util";
 
 const moreInfoData = {
-  title: "Globefarer offers global shipping",
-  description:
-    "Providing independent advice and identifying the right fit for you. Services are sourced.",
-  href: "#",
-  buttonText: "about our services",
-  imageSrc: `${prefix}08/cargo-home-img-3.jpg`,
-  imageAlt: "Image showing global shipping",
+    title: "Globefarer offers global shipping",
+    description:
+        "Providing independent advice and identifying the right fit for you. Services are sourced.",
+    href: "#",
+    buttonText: "about our services",
+    imageSrc: `${prefix}08/cargo-home-img-3.jpg`,
+    imageAlt: "Image showing global shipping",
 };
 
 export default function Home() {
-  return (
-    <>
-      <MobileHeader />
-      {/* Mobile navigation */}
-      <MainHeader />
-      {/* Main navigation */}
-      <div className="-mt-[8rem] lg:-mt-[11.3rem] h-full w-full relative block">
-        <main className="relative inline-block w-full align-top">
-          <HeroSection />
-          <OurSolutions />
-          <OurServicesColumns />
-          <Testimonials />
-          <Counters />
-          <MoreInfo data={moreInfoData} />
-          <OurDevelopers />
-          <Frame />
-          <PricingRange />
-          {/* <CompanyLogos /> */}
-        </main>
-      </div>
-      {/* <Footer /> */}
-    </>
-  );
+    return (
+        <>
+            <MobileHeader/>
+            {/* Mobile navigation */}
+            <MainHeader/>
+            {/* Main navigation */}
+            <div className="-mt-[8rem] lg:-mt-[11.3rem] h-full w-full relative block">
+                <main className="relative inline-block w-full align-top">
+                    <HeroSection/>
+                    <OurSolutions/>
+                    <OurServicesColumns/>
+                    <Testimonials/>
+                    <Counters/>
+                    <MoreInfo data={moreInfoData}/>
+                    <OurDevelopers/>
+                    <Frame/>
+                    <PricingRange/>
+                    <CompanyLogos/>
+                </main>
+            </div>
+            {/* <Footer /> */}
+        </>
+    );
 }
