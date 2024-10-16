20e85cc7f3d61b11c9422039059531a68a5c5804
godwin <65357127+macqueen006@users.noreply.github.com>
2024-10-16 07:22:12 +0100

    completed the Custom Cursor

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  reactStrictMode: false,
   basePath: process.env.NODE_ENV === 'production' ? '/cargo' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/cargo/' : '',
   webpack(config) {
